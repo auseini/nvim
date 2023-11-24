0e41dcc027d5bc1222d929833910bc76b2c154df
Arian Useini <auseini20@gmail.com>
2023-11-23 21:10:12 -0600

    add cmake lsp,  save remap

--- lua/arianuseini/remap.lua
+++ lua/arianuseini/remap.lua
@@ -43,6 +43,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/arianuseini/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+vim.keymap.set("n", "zz", vim.cmd.update);
+
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
